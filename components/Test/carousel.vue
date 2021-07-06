1d49d9663509930e7edf10cf6871bd70287dab92
Aishwary Saxena <aish2saxena@gmail.com>
2021-07-06 12:11:29 +0530

    todo component

--- components/Test/carousel.vue
+++ components/Test/carousel.vue
@@ -1,4 +1,19 @@
-<template> </template>
+<template>
+  <v-container>
+    <carousel
+      :per-page="1"
+      :navigate-to="someLocalProperty"
+      :mouse-drag="false"
+    >
+      <slide>
+        Slide 1 Content
+      </slide>
+      <slide>
+        Slide 2 Content
+      </slide>
+    </carousel>
+  </v-container></template
+>
 <style lang="scss" scoped></style>
 <script>
 import { Carousel, Slide } from "vue-carousel";
